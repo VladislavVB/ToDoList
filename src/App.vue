ff352a97457f724e88a5d54c195dfb90cd6df7ce
Vlad Vlad <burdukovskij.vlad@mail.ru>
2021-10-30 17:41:52 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  /* text-align: center; */
+  align-items: center;
+  min-height: 100vh;
+  background-color: #313131;
+}
+</style>
