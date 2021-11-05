0ef4cf568522b1d91738654f4b0aac4ddb6fc25e
Vlad Vlad <burdukovskij.vlad@mail.ru>
2021-11-05 20:57:33 +0300

    чекпоинт

---
+++ src/pages/toDoList/components/CreateItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="content-add">
+    <input
+      @keydown.enter="addItem()"
+      v-model="listItemName"
+      type="text"
+      placeholder="text"
+    />
+    <button :class="listItemName != '' ? '' : 'deactive'" @click="addItem()">
+      Add
+    </button>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/toDoList/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content-body">
+    <div v-for="item in listItems" :key="item" class="content-body-item">
+      <p v-if="item.edit === false">{{ item.title }}</p>
+      <div v-else>
+        <input type="text" v-model="item.title" /><span
+          class="save"
+          @click="editItem(item)"
+          >save</span
+        >
+      </div>
+      <div class="controls">
+        <span @click="item.edit = true" class="editor"
+          ><img src="@/assets/edit.png" alt=""
+        /></span>
+        <span @click="itemDelite(item)" class="close">✖</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
