b8fed13cf06d107d450dd4801e3d719e44b5460d
Vlad Vlad <burdukovskij.vlad@mail.ru>
2021-11-07 16:04:48 +0300

    ну чуть лучше

---
+++ src/pages/toDoList/components/AddItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="content-add">
+    <input
+      @keydown.enter="addItem()"
+      v-model="listItemName"
+      type="text"
+      placeholder="text"
+    />
+    <button :class="listItemName != '' ? '' : 'deactive'" @click="addItem()">
+      Add
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddItem",
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      listItemName: ''
+      // listItemName: this.add_prop
+    }
+  },
+  methods() {
+
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
